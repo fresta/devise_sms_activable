9b7c684b088c4768acf90ee7c66c08a356d270e3
Anthony Fresta <afresta@exegy.com>
2018-01-23 13:03:14 -0600

    added twilio

--- lib/devise_sms_activable/sms_sender.rb
+++ lib/devise_sms_activable/sms_sender.rb
@@ -2,7 +2,7 @@ class Devise::SmsSender
   #Actually sends the sms token. feel free to modify and adapt to your provider and/or gem
   def self.send_sms(phone,message)
     @client = Twilio::REST::Client.new
-    sms = @client.messages.create(from: TWILIO_NUMBER, to: phone, body: message)
+    sms = @client.messages.create(from: Twilio.from_number, to: phone, body: message)
  #   sms = Moonshado::Sms.new(phone, message)
     return sms
   end
